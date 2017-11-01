b5f215a673f09c52542b1e5e43e65c26e63b703b
Emily Morgan <emorgan05@gmail.com>
2017-11-01 16:25:57 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,8 +14,11 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @number = params[:number].to_i
     @phrase = params[:phrase]
+    @string = ""
     @number.times do |x|
-      "My catchphrase is #{@phrase}!"
+      @string += @phrase
     end
   end
+
+  
 end
