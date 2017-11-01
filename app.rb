43238062496d45e59ae8183f7ae15b956ad30548
Emily Morgan <emorgan05@gmail.com>
2017-11-01 15:56:57 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  
+  get '/reversename/:name' do
+    @name = params[:name].reverse
+    "Your name must be #{@name}!"
+  end
 
 end
