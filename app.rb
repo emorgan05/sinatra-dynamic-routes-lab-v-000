c07f804ba8adafa6246ad81f6bc7993f659950bf
Emily Morgan <emorgan05@gmail.com>
2017-11-01 16:40:19 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -26,6 +26,7 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
+    @num1 = params[:number1].to_i
     if params[:operation] == "add"
       "#{params[:number1].to_i} + #{params[:number2].to_i}"
     elsif params[:operation] == "subtract"
