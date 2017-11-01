5a2aa85c9d519594c728390af56ebc62048f50bc
Emily Morgan <emorgan05@gmail.com>
2017-11-01 16:12:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
 
   get '/say/:number/:phrase' do
     @num = params[:number].to_i
-    @phrase = params[:phrase].gsub("%20", " ")
+    @phrase = params[:phrase]
     @num.times do |x|
       "#{@phrase}!"
     end
